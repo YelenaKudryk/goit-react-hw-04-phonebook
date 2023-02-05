4b70d3b894c15e101e68bce0fa67a6d57e27f9cd
YelenaKudryk <kudrykolena@gmail.com>
2023-02-05 21:41:05 +0200

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,19 @@
-import { Component } from 'react';
+import { useState } from 'react';
+import useLocalStorage from 'hooks/useLocalStorage';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-// import contacts from 'data/data';
 import { nanoid } from 'nanoid';
 import { Notify } from 'notiflix';
 import { PageTitle, Text, Section } from './App.styled';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const App = () => {
+  const [contacts, setContacts] = useLocalStorage('my-contacts', []);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('my-contacts'));
-    if (contacts?.length) {
-      this.setState({ contacts });
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
-
-    if (prevState.contacts.length !== contacts.length) {
-      localStorage.setItem('my-contacts', JSON.stringify(contacts));
-    }
-  }
-
-  addContact = ({ name, number }) => {
-    if (this.isDublicate(name, number)) {
-      Notify.failure(`${name} or ${number} is already in contacts.`);
-      return false;
-    }
-    this.setState(({ contacts }) => {
-      const newContact = {
-        id: nanoid(),
-        name,
-        number,
-      };
-      return { contacts: [newContact, ...contacts] };
-    });
-    return true;
-  };
-
-  isDublicate = (name, number) => {
+  const isDublicate = (name, number) => {
     const normalizedName = name.toLowerCase();
     const normalizedNumber = number.trim();
-    const { contacts } = this.state;
     const contact = contacts.find(({ name, number }) => {
       return (
         name.toLowerCase() === normalizedName || normalizedNumber === number
@@ -56,19 +22,33 @@ class App extends Component {
     return Boolean(contact);
   };
 
-  deleteContact = id => {
-    this.setState(({ contacts }) => {
-      const newContacts = contacts.filter(contact => contact.id !== id);
-      return { contacts: newContacts };
+  const addContact = ({ name, number }) => {
+    if (isDublicate(name, number)) {
+      Notify.failure(`${name} or ${number} is already in contacts.`);
+      return false;
+    }
+    setContacts(prevContacts => {
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+      return [newContact, ...prevContacts];
     });
+    return true;
   };
 
-  handleFilter = ({ target }) => {
-    this.setState({ filter: target.value });
+  const deleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
   };
 
-  getFilteredContacts = () => {
-    const { filter, contacts } = this.state;
+  const handleFilter = ({ target }) => {
+    setFilter(target.value);
+  };
+
+  const getFilteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     const result = contacts.filter(({ name, number }) => {
       return (
@@ -79,35 +59,29 @@ class App extends Component {
     return result;
   };
 
-  render() {
-    const { deleteContact, addContact, handleFilter, getFilteredContacts } =
-      this;
-    const { contacts, filter } = this.state;
-    const isContacts = Boolean(contacts.length);
-    const filtredContacts = getFilteredContacts();
+  const isContacts = Boolean(contacts.length);
 
-    return (
-      <>
-        <Section>
-          <PageTitle>Phonebook</PageTitle>
-          <ContactForm onSubmit={addContact} />
-        </Section>
+  return (
+    <>
+      <Section>
+        <PageTitle>Phonebook</PageTitle>
+        <ContactForm onSubmit={addContact} />
+      </Section>
 
-        <Section>
-          <PageTitle>Contacts</PageTitle>
-          <Filter value={filter} onChange={handleFilter} />
-          {isContacts ? (
-            <ContactList
-              deleteContact={deleteContact}
-              contacts={filtredContacts}
-            />
-          ) : (
-            <Text>Your phonebook is empty</Text>
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+      <Section>
+        <PageTitle>Contacts</PageTitle>
+        <Filter value={filter} onChange={handleFilter} />
+        {isContacts ? (
+          <ContactList
+            deleteContact={deleteContact}
+            contacts={getFilteredContacts()}
+          />
+        ) : (
+          <Text>Your phonebook is empty</Text>
+        )}
+      </Section>
+    </>
+  );
+};
 
 export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,67 +1,66 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Form, Label, Input, Button } from './ContactForm.styled';
 import PropTypes from 'prop-types';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = ({ target }) => {
+    const { value } = target;
+    setName(value);
   };
 
-  handleChange = ({ target }) => {
-    const { name, value } = target;
-    this.setState({ [name]: value });
+  const handleNumberChange = ({ target }) => {
+    const { value } = target;
+    setNumber(value);
   };
 
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    const addedContact = this.props.onSubmit({ ...this.state });
+    const addedContact = onSubmit({ name, number });
     if (addedContact) {
-      this.resetForm();
+      resetForm();
     }
   };
 
-  resetForm = () => {
-    this.setState({ name: '', number: '' });
+  const resetForm = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { handleChange, handleSubmit } = this;
-    const { name, number } = this.state;
-
-    return (
-      <Form onSubmit={handleSubmit}>
-        <Label>
-          Name
-          <Input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            placeholder="Name"
-            value={name}
-            onChange={handleChange}
-          />
-        </Label>
-        <Label>
-          Number
-          <Input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            placeholder="Number"
-            value={number}
-            onChange={handleChange}
-          />
-        </Label>
-        <Button type="submit">Add contact</Button>
-      </Form>
-    );
-  }
-}
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Label>
+        Name
+        <Input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          placeholder="Name"
+          value={name}
+          onChange={handleNameChange}
+        />
+      </Label>
+      <Label>
+        Number
+        <Input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          placeholder="Number"
+          value={number}
+          onChange={handleNumberChange}
+        />
+      </Label>
+      <Button type="submit">Add contact</Button>
+    </Form>
+  );
+};
 
 export default ContactForm;
 
